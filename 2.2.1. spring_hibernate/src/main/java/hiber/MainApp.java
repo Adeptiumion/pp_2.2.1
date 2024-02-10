f58fdb92523d28aa9f1b37fc817f2286d89fbb2b
Adept <adeptiumion@gmail.com>
2024-02-10 16:24:46 +0700

    fixCommitName

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.logging.Logger;
 
 public class MainApp {
+    // Для проверки
     private static final Logger appLogger = Logger.getLogger(MainApp.class.getName());
 
     public static void main(String[] args) throws SQLException {
